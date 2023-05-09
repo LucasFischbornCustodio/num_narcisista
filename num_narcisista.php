5f0ae642f547e7ff7be50f0da5254d24c0c74ffd
LucasFischbornCustodio <93406817+LucasFischbornCustodio@users.noreply.github.com>
2023-05-09 11:58:22 -0300

    Add files via upload

---
+++ num_narcisista.php
@@ -0,0 +1,32 @@
+<?php
+
+
+
+
+
+
+
+$retalho = (str_split($inteiro = (int)readline('Digite um número: ')));
+$separado = count($retalho);
+intval($separado);
+print($separado);
+print_r($retalho);
+
+pow($retalho, $separado);
+//** eleva a potencia = a valor definido */
+
+
+
+
+
+
+
+
+
+//permitir usuário inserir um valor inteiro (??????) e armazenar em uma variavel
+//transformar esse valor inteiro em string e usar explode para fazer sua decomposição
+//transformar essa strings em valores inteiros
+//elevar cada numero na potencia de inteiros na tela
+//somar numeros
+//imprimir na tela
+?>
